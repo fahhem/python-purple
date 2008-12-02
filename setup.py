35a2297f5733c330829b9a33504aed180992c9bb
Ragner Magalhaes <ragner.magalhaes@openbossa.org>
2008-12-02 20:40:51 +0000

    Added setup descriptions.
    FIXES:
     - Added setup descriptions.
    
    Signed-off-by: Bruno Abinader <bruno.abinader@indt.org.br>
    
    git-svn-id: https://garage.maemo.org/svn/carman/branches/carman-0.7-beta2/python-purple@1337 596f6dd7-e928-0410-a184-9e12fd12cf7e

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+
 import sys
 import os
-from glob import glob
+
+from ez_setup import use_setuptools
+use_setuptools('0.6c8')
+
 
 from setuptools import setup, find_packages, Extension
 from distutils.sysconfig import get_python_inc
+from glob import glob
+
 from Cython.Distutils import build_ext
 
 from subprocess import Popen, PIPE
@@ -12,20 +18,30 @@ from subprocess import Popen, PIPE
 cflags = Popen(['pkg-config', '--cflags', 'purple'], stdout=PIPE).communicate()[0].split()
 ldflags = Popen(['pkg-config', '--libs', 'purple'], stdout=PIPE).communicate()[0].split()
 
+purplemodule = Extension('purple',
+                         sources=['c_purple.c','purple.pyx'],
+                         depends=glob('libpurple/*.pxd'),
+                         extra_compile_args=cflags,
+                         extra_link_args=ldflags)
+
+long_description = """\
+Python bindings for Purple, a multi-protocol instant messaging library.
+"""
+
 class pypurple_build_ext(build_ext):
     def finalize_options(self):
         build_ext.finalize_options(self)
         self.include_dirs.insert(0, 'libpurple')
         self.pyrex_include_dirs.extend(self.include_dirs)
 
-setup(
-    name = 'python-pypurple',
-    version = '0.1',
-    author ='Bruno Abinader',
-    author_email='bruno.abinader@openbossa.org',
-    cmdclass = {'build_ext': pypurple_build_ext},
-    ext_modules=[Extension('purple',
-        sources=['c_purple.c','purple.pyx'],
-        depends=glob('libpurple/*.pxd'),
-        extra_compile_args=cflags,
-        extra_link_args=ldflags)])
+setup(name = 'python-purple',
+      version = '0.1',
+      author ='Bruno Abinader',
+      author_email = 'bruno.abinader@openbossa.org',
+      description = 'Python bindings for Purple',
+      long_description = long_description,
+      packages = find_packages(),
+      ext_modules = [purplemodule],
+      zip_safe=False,
+      cmdclass = {'build_ext': pypurple_build_ext},
+      )
